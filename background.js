cc092cdc0e3356bff68076850ae9270db1823912
Banee Ishaque K <Baneeishaque@gmail.com>
2018-07-08 18:23:47 +0530

    ok

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-//TODO : Protocol Inclusion
+//TODO : Clean code
 
 // Set up the download trigger
 chrome.downloads.onDeterminingFilename.addListener(function (downloadItem) {
@@ -56,14 +56,8 @@ function generate_url_and_add_aria2_remote_task(e) {
 
 function add_aria2_remote_task(url)
 {
-//    var PostUrl = "http://vfmob.com.md-in-64.webhostbox.net/wp-production/aria2_remote_server/http_API/insert_Task.php?url=";
-    var PostUrl = "http://vfmob.com.md-in-64.webhostbox.net/wp-production/aria2_remote_server/http_API/insert_Task_patch.php?url=";
-
-    //TODO : Encode URL instead of replace
-//    url = url.replace("https://", "");
-//    url = url.replace("http://", "");
-//    url = url.replace("ftps://", "");
-//    url = url.replace("ftp://", "");
+    var PostUrl = "http://vfmob.com.md-in-64.webhostbox.net/wp-production/aria2_remote_server/http_API/insert_Task.php?url=";
+//    var PostUrl = "http://vfmob.com.md-in-64.webhostbox.net/wp-production/aria2_remote_server/http_API/insert_Task_patch.php?url=";
 
     console.log("Download URL : " + url);
     
@@ -71,7 +65,6 @@ function add_aria2_remote_task(url)
     console.log("Encoded Download URL : " + url);
     
     PostUrl += url;
-//    PostUrl += encodeURIComponent(url);
 
     console.log("Post URL : " + PostUrl);
 
